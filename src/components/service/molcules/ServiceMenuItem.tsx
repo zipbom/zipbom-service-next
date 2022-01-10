6cd68d1585fd1f86e6341bb6b61a166f0d2bfb11
sujin <sujin.k0402@gmail.com>
2022-01-10 16:01:48 +0900

    feat: theme store test

--- src/components/service/molcules/ServiceMenuItem.tsx
+++ src/components/service/molcules/ServiceMenuItem.tsx
@@ -1,28 +1,33 @@
-import ThemeStore from "@stores/ThemeStore";
 import { BellOutlined, SearchOutlined, MenuOutlined } from "@ant-design/icons";
 import { Menu, Radio } from "antd";
 import Link from "next/link";
+import { useCoreStores } from "zipbom-core-react-lib";
+import { useObserver } from "mobx-react";
 
 const ServiceMenuItem: React.FC = () => {
+  const { themeStore } = useCoreStores();
+
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     console.log("radio checked", e.target.value);
-    // setValue(e.target.value);
-    ThemeStore.setTheme(e.target.value);
+    themeStore.getThemeName();
+    // themeStore.setTheme(e.target.value);
   };
-  return (
+
+  return useObserver(() => (
     <>
       <Menu.Item>
         <BellOutlined />
         <span>집봄</span>
         <SearchOutlined />
         <MenuOutlined />
-        <Radio.Group onChange={onChange} value={ThemeStore.themeName}>
+        {themeStore.curThemeName}
+        <Radio.Group onChange={onChange} value={themeStore.curThemeName}>
           <Radio value="light">라이트 테마</Radio>
           <Radio value="dark">다크 테마</Radio>
         </Radio.Group>
       </Menu.Item>
     </>
-  );
+  ));
 };
 
 export default ServiceMenuItem;
